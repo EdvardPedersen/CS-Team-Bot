6c689c6ab1a620fde949accadfedbb26ac21f7de
Øyvind <oeyvindnohr@gmail.com>
2022-02-11 02:07:30 +0100

    feat: add signup of new team members, optout of leaving teammembers, end and cancellation of ongoing registrations for a match. Match registration can now be initialized as next default match day \!register or a given date: \!register yyyy-mm-dd. \!next shows next coming match date and participant list. \!upcoming shows all upcoming matches, chronologically, only dates. \!easterEgg clears broadcast channel. Admin configuration includes:\!admins: list admins, \!setAdmin: admin can set new admin, \!removeAdmin:admin can remove admins, not including owner. \!purge yyyy-mm-dd removes a match registration completely

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 
 import pickle
+from random import randint
+
 import discord
 import datetime
 '''
@@ -25,7 +27,9 @@ Interface:
 
 class Configuration():
     def __init__(self, admin, server, role):
-        self.super_user = admin
+        self.owner = int(input("Insert your discordID\n"))
+        self.super_users_id = admin
+        self.super_users_id.append(self.owner)
         self.server = server
         self.team_role = role
         self.guild = ""
@@ -33,31 +37,33 @@ class Configuration():
 
 
 class Registration():
-    def __init__(self,users) -> None:
+    def __init__(self,users,date=None) -> None:
         self.date = None
-        self.playdate_next()
+        self.playdate_next(date)
         self.users = users
         self.status = False
 
-    def playdate_next(self):
-        playday = "Wednesday"
-        weekdays = {
-            'Monday':0,
-            'Tuesday':1,
-            'Wednesday':2,
-            'Thursday':3,
-            'Friday':4,
-            'Saturday':5,
-            'Sunday':6,
-        }
-        days_in_week = 7
-        today = datetime.date.today()
-        # weekday 2 = wednesday, please change this magic number
-        days_to = weekdays[playday] - datetime.date.weekday(today)
-        if days_to <= 0: # we are past playday this week
-            days_to += days_in_week
-        self.date =  today + datetime.timedelta(days_to)
-
+    def playdate_next(self,date):
+        if date:
+            self.date = date
+        else:
+            playday = "Wednesday"
+            weekdays = {
+                'Monday':0,
+                'Tuesday':1,
+                'Wednesday':2,
+                'Thursday':3,
+                'Friday':4,
+                'Saturday':5,
+                'Sunday':6,
+            }
+            days_in_week = 7
+            today = datetime.date.today()
+            # weekday 2 = wednesday, please change this magic number
+            days_to = weekdays[playday] - datetime.date.weekday(today)
+            if days_to <= 0: # we are past playday this week
+                days_to += days_in_week
+            self.date =  today + datetime.timedelta(days_to)
 
     def set_active(self):
         self.status = True
@@ -83,7 +89,8 @@ class CsBot(discord.Client):
         if self.record:
             least = list(self.record.keys())[0]
             for record in self.record.keys():
-                if record > least:
+                if record >= datetime.date.today() and record < least:
+                    print(record)
                     least = record
             self.registration_previous = self.record[least]
 
@@ -119,6 +126,8 @@ class CsBot(discord.Client):
             if await self.get_permissions(reaction.member) > 1:
                 if reaction.message_id == self.registration_post.id:
                     self.registration_next.users[reaction.user_id] = reaction.member.name
+            else:
+                await reaction.member.send("You have not signed up for the coming season: <Follow this guideline>")
 
     async def on_raw_reaction_remove(self, reaction):
         '''
@@ -136,14 +145,26 @@ class CsBot(discord.Client):
         if message.author == self.user:
             return
         permissions = await self.get_permissions(message.author)
-        if message.content.startswith('!r'):
+        if message.content == "!register":
             if permissions == 3:
                 if self.registration_active:
                     await message.author.send("Registration already active, please cancel previous registration to start a new.")
                 else:
                     await self.registration_start()
 
-        if message.content.startswith('!cancel'):
+        elif message.content.startswith("!register "):
+            if permissions == 3:
+                if self.registration_active:
+                    await message.author.send("Registration already active, please cancel previous registration to start a new.")
+                else:
+                    await self.registration_start(message.content.removeprefix("!register "))
+
+        if message.content == "!signup":
+            await message.author.add_roles(await self.get_role())
+
+        if message.content == "!optout":
+            await message.author.remove_roles(await self.get_role())
+        if message.content.startswith("!cancel"):
             if permissions == 3:
                 if self.registration_active:
                     await self.registration_cancel()
@@ -163,27 +184,80 @@ class CsBot(discord.Client):
             else:
                 await self.match_print_next()
 
+        if message.content == "!upcoming":
+            upcoming = ["**"+ upcoming.date.strftime("%Y-%m-%d")+"**" for upcoming in self.record.values() if upcoming.date > datetime.date.today()]
+            upcoming = sorted(upcoming)
+            await self.broadcast_channel.send(f"Registrated upcoming matches: {upcoming}")
+
         if message.content == "!maps":
             if permissions > 1:
                 await message.author.send("You can register preferences here")
             else:
                 await message.author.send("You do not have permission to register map preferences")
 
+        if message.content.startswith("!purge"):
+            if permissions == 3:
+                await self.registration_purge(message.content)
+
+        if message.content == "!easterEgg":
+            if permissions == 3:
+                await self.broadcast_channel.purge()
+
         if message.content == "!dank":
             if permissions > 1:
-                await self.broadcast_channel.send("Brought to you by HenkeBazZ\nhttps://www.youtube.com/watch?v=q6EoRBvdVPQ&list=PLFsQleAWXsj_4yDeebiIADdH5FMayBiJo")
+                await self.broadcast_channel.send(f"Brought to you by HenkeBazZ\nhttps://www.youtube.com/watch?v=q6EoRBvdVPQ&list=PLFsQleAWXsj_4yDeebiIADdH5FMayBiJo&index=1")
+    
+        if message.content.startswith("!setAdmin"):
+            if permissions == 3:
+                IDs = message.content.removeprefix("!setAdmin ")
+                IDs = IDs.split()
+                for user in IDs:
+                    self.config.super_users_id.append(int(user))
 
-    async def registration_start(self):
+        if message.content.startswith("!removeAdmin"):
+            if permissions == 3:
+                IDs = message.content.removeprefix("!removeAdmin")
+                IDs = IDs.split()
+                for id in IDs:
+                    if int(id) != self.config.owner:
+                        self.config.super_users_id.remove(int(id))
+
+        if message.content == "!admins":
+            admins = [admin.name for admin in self.get_all_members() if admin.id in self.config.super_users_id]
+            await self.broadcast_channel.send(f"Adminlist: {admins}")
+
+
+    async def registration_start(self,date=None):
         '''
         Start a registration for next playdate
         If date for next playdate matches the previous one, the previous registration 
         have to be manually deleted before trying again
         '''
-        self.registration_next = Registration({})
+        if date:
+            year,month,day = date.split('-')
+            date = datetime.date(int(year),int(month),int(day))
+        self.registration_next = Registration({},date)
         self.registration_active = True
-        self.registration_post = await self.broadcast_channel.send("Please react to this post to sign up for the next match.")
+        self.registration_post = await self.broadcast_channel.send(f"```cpp\n# please react to this post to sign up for the match on:\n{self.registration_next.date}```")
+
+
+    async def registration_purge(self, message):
+        message = message.removeprefix("!purge ")
+        year,month,day = message.split('-')
+        date = datetime.date(int(year),int(month),int(day))
+        try:
+            self.record.pop(date)
+            if self.registration_previous.date == date:
+                self.registration_previous = None
+                await self.set_registration_previous()
+        except KeyError:
+            print("Invalid purge date")
+        await self.write_prev_state()
 
     async def registration_cancel(self):
+        '''
+        Cancel the ongoing registration
+        '''
         self.registration_next = None
         await self.registration_post.delete()
         self.registration_post = None
@@ -194,7 +268,7 @@ class CsBot(discord.Client):
         if self.registration_previous.date < datetime.date.today():
             await self.broadcast_channel.send("No planned match")
         else:
-            await self.broadcast_channel.send(f"\nPlaydate: **[{self.registration_previous.date}]**\nPlaying: {playing}")
+            await self.broadcast_channel.send(f"Date: **[{self.registration_previous.date}]**\nPlaying: {playing}")
 
     async def read_prev_state(self):
         try:
@@ -232,7 +306,7 @@ class CsBot(discord.Client):
 
 
     async def get_permissions(self, user):
-        if user.id == self.config.super_user:
+        if user.id in self.config.super_users_id:
             return 3
         if user in self.config.role.members:
             return 2
@@ -241,11 +315,21 @@ class CsBot(discord.Client):
 
 
 token = ""
-with open("auth") as f:
-    token = f.read()
+try:
+    with open("auth") as f:
+        token = f.read()
+except FileNotFoundError:
+    print("Unable to read authToken")
+
+admins = []
+try:
+    with open("admins") as f:
+        for admin in f.readlines():
+            admins.append(int(admin))
+except FileNotFoundError:
+    print("No admins added, only owner has access to core features")
 
 
-
-config = Configuration(154310949195481088, 875845075068944424, 941396110252060702)
+config = Configuration(admins, 875845075068944424, 941396110252060702)
 client = CsBot(config)
 client.run(token)
