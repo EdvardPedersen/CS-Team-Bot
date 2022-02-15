083cd2251b3b3bde260b71cb21f8986d7086a707
Nohr <56138637+Askepios-ai@users.noreply.github.com>
2022-02-15 17:20:26 +0100

    Update stupid.py
    Moved comments to comply pep

--- stupid.py
+++ stupid.py
@@ -7,16 +7,16 @@ class StupidityHandler(GenericMessageHandler):
     def __init__(self,help_text,response,reply_private=False):
         super().__init__(help_text,response,reply_private)
 
-    ''' Returns a random dad joke '''
     def dad_jokes(self):
+        ''' Returns a random dad joke '''
         msg = Dadjoke().joke
         return(msg)
 
     async def message_dad(self, message, permission):
         await self.reply(message, self.dad_jokes())
 
-    ''' Returns a random dank video from a playlist on youtube '''
     def dank(self):
+        ''' Returns a random dank video from a playlist on youtube '''
         playlist = Playlist('https://www.youtube.com/watch?v=q6EoRBvdVPQ&list=PLFsQleAWXsj_4yDeebiIADdH5FMayBiJo')
         yt = random.choice(playlist.videos)
         return(f"https://www.youtube.com/watch?v={yt.video_id}&list=PLFsQleAWXsj_4yDeebiIADdH5FMayBiJo")
